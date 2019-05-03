fe104a0329e4236d71014ad44ddfdb0f5b04bee4
Douglas Roberts <daroberts@quantaservices.com>
2019-05-03 14:43:35 -0500

    added new file

---
+++ test2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Test 2 PHP page</title>
+  
+</head>
+
+<body>
+  <?php echo "Hello World!" ?>
+  <p>
+  <?= "This is PHP" ?>
+</body>
